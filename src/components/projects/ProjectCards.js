9bf56027c6efbd28baddb779f656236ce15fc3bb
kabirfaisal1 <md.kabirfaisal@outlook.com>
2023-04-13 17:10:23 -0400

    fixed all the project card page

--- src/components/projects/ProjectCards.js
+++ src/components/projects/ProjectCards.js
@@ -7,7 +7,7 @@ import { BsGithub } from "react-icons/bs";
 function ProjectCards(props) {
   return (
     <Card data-test-id={props.title} className="project-card-view">
-      <Card.Img variant="top" src={props.imgPath} alt="card-img" />
+      <Card.Img variant="top" src={props.imgPath} alt={props.alt} />
       <Card.Body>
         <Card.Title>{props.title}</Card.Title>
         <Card.Text style={{ textAlign: "justify" }}>
@@ -19,7 +19,7 @@ function ProjectCards(props) {
         </Button>
         {"\n"}
         {"\n"}
-        {!props.isBlog && props.siteLink && (
+        {props.siteLink &&(
           <Button
             variant="primary"
             href={props.siteLink}
--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,27 +1,16 @@
 import React from "react";
-import { Container, Row, Col, Card, Button } from "react-bootstrap";
+import { Container, Row, Col} from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
-import Particle from "../Particle";
+
 import projectItem from "./projectData"
-// import noteTaker from "../../assets/Projects/noteTake.png";
-// import shoppers from "../../assets/Projects/shopper.png";
-// import emTracker from "../../assets/Projects/EmployeeTracker.png";
-// import teamProfile from "../../assets/Projects/teamProfile.png";
-// import workDay from "../../assets/Projects/WorkDay.png";
-// import techBlog from "../../assets/Projects/techBlog.png";
-// import weather from "../../assets/Projects/weather.png";
-import jsQuiz from "../../assets/Projects/jsQuiz.png";
-// import socialNetwork from "../../assets/Projects/socialNetwork.png";
-// import pwgenerator from "../../assets/Projects/pwgenerator.png";
-import { AiFillGithub, AiFillLinkedin } from "react-icons/ai";
-import { RiProfileLine } from "react-icons/ri";
 
 
 
 function Projects() {
     return (
+
         <div style={{ fontSize: 'large' }}>
-           
+
             <Container>
                 <h1 className="project-heading">
                     My <strong className="purple">Projects </strong>
@@ -29,14 +18,16 @@ function Projects() {
                 <p style={{ color: "white" }}>
                     Here are a few projects I've worked on recently.
                 </p>
+
                 <Row data-test-id="project-cards" style={{ justifyContent: "center", paddingBottom: "10px" }}>
 
                     {projectItem.cardData.map((project, index) => {
                         return (
-                            <Col md={4} className="project-card">
+                            <Col key={index} md={4} className="project-card">
 
                                 <ProjectCard
-                                    imgPath={jsQuiz}
+                                    imgPath={project.img}
+                                    alt={`img id:${project.id} title:${project.title}`}
                                     isBlog={false}
                                     title={project.title}
                                     description={project.description}
@@ -52,5 +43,5 @@ function Projects() {
         </div>
     )
 };
+
 export default Projects;
-console.log(projectItem.cardData)
