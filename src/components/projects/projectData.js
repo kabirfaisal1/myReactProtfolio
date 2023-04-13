317e5cbf2512338973401dc58e5b5b9085bc2b24
kabirfaisal1 <md.kabirfaisal@outlook.com>
2023-04-13 01:00:21 -0400

    re Structure projects

---
+++ src/components/projects/projectData.js
@@ -0,0 +1,45 @@
+const Cardcontact = {
+    cardData: [
+      {
+        id: '1',
+        title: 'JavaScript Algorithms',
+       
+          description: 'Worked on a application that build a timed coding quiz with multiple-choice questions for javascript. This app will run in the browser, and will feature dynamically updated HTML and CSS powered by JavaScript code and store users score in localStorage.',
+        siteLink: 'https://kabirfaisal1.github.io/JavaScriptQuiz/',
+        ghLink: 'https://github.com/kabirfaisal1/JavaScriptQuiz.git'
+      },
+      {
+        id: '2',
+        title: 'Prize Wizard',
+       
+        description: 'Worked on a application that build a timed coding quiz with multiple-choice questions for javascript. This app will run in the browser, and will feature dynamically updated HTML and CSS powered by JavaScript code and store users score in localStorage.',
+        siteLink: 'https://kabirfaisal1.github.io/JavaScriptQuiz/',
+        ghLink: 'https://github.com/kabirfaisal1/JavaScriptQuiz.git'
+      },
+      {
+        id: '3',
+        title: 'Note Taker',
+       
+        description: 'Worked on a application that build a timed coding quiz with multiple-choice questions for javascript. This app will run in the browser, and will feature dynamically updated HTML and CSS powered by JavaScript code and store users score in localStorage.',
+        siteLink: 'https://kabirfaisal1.github.io/JavaScriptQuiz/',
+        ghLink: 'https://github.com/kabirfaisal1/JavaScriptQuiz.git'
+      }, {
+        id: '4',
+        title: 'Tech-Blog',
+       
+        description: 'Worked on a application that build a timed coding quiz with multiple-choice questions for javascript. This app will run in the browser, and will feature dynamically updated HTML and CSS powered by JavaScript code and store users score in localStorage.',
+        siteLink: 'https://kabirfaisal1.github.io/JavaScriptQuiz/',
+        ghLink: 'https://github.com/kabirfaisal1/JavaScriptQuiz.git'
+      },  {
+        id: '5',
+        title: 'JavaScript Quiz',
+       
+          description: 'Worked on a application that build a timed coding quiz with multiple-choice questions for javascript. This app will run in the browser, and will feature dynamically updated HTML and CSS powered by JavaScript code and store users score in localStorage.',
+        siteLink: 'https://kabirfaisal1.github.io/JavaScriptQuiz/',
+        ghLink: 'https://github.com/kabirfaisal1/JavaScriptQuiz.git'
+      },
+     
+    ]
+  }
+  
+  export default Cardcontact
