eaf1f7f36e556201bbaad45bae3b901fe4f92599
kabirfaisal1 <md.kabirfaisal@outlook.com>
2023-04-13 22:42:14 -0400

    spell check fixed

--- src/components/projects/projectData.js
+++ src/components/projects/projectData.js
@@ -64,7 +64,7 @@ const cardContact = {
       id: "8",
       title: "WorkDay Scheduler",
       img: "https://kineticsoftware.com/wp-content/uploads/2019/09/schedule-1.png",
-      description: "Createing a simple calendar application that allows a user to save events for each hour of the day. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.",
+      description: "Creating a simple calendar application that allows a user to save events for each hour of the day. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.",
       siteLink: "https://kabirfaisal1.github.io/WorkDayScheduler/",
       ghLink: "https://github.com/kabirfaisal1/WorkDayScheduler.git"
     },
